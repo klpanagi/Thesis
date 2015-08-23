7327bd8c70fe57ee6996cbc646e68560fa2b7af8
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-08-23 23:42:35 +0300

    Fixes

---
+++ jetson-tk1/opencv_onlyGPU/opencv.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# buildOpenCV.sh
+# Builds and installs OpenCV for the Jetson TK1
+# Accelerates OpenCV by using GPU routines
+# L4T 21.X
+# This version is for CUDA 6.5
+# OpenCV 2.4.10
+# Note: Earlier versions of OpenCV (< 2.4.10) are not compatible with CUDA 6.5
+
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-get update
+
+# Install OpenCV dependencies
+sudo apt-get -y install \
+    build-essential checkinstall cmake pkg-config yasm
+
+# Video I/O
+sudo apt-get -y install \
+    libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
+
+# For building Python wrappers
+sudo apt-get -y install \
+    python-dev python-numpy
+
+sudo apt-get -y install \
+    libfaac-dev libjack-jackd2-dev \
+      libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev \
+        libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev \
+          libxvidcore-dev texi2html zlib1g-dev
+
+NUM_THREADS=4
+ver=2.4.10
+
+git clone https://github.com/Itseez/opencv.git opencv-$ver
+cd opencv-$ver
+git checkout $ver
+mkdir build
+cd build
+
+# Jetson architecture is 3.2
+FLAGS=" -DWITH_CUDA=ON -DCUDA_ARCH_BIN=3.2 -DBUILD_TESTS=ON "
+FLAGS+=" -DBUILD_PERF_TESTS=ON -DBUILD_EXAMPLES=ON "
+FLAGS+=" -DCMAKE_INSTALL_PREFIX=/opt/opencv-gpu_only "
+
+cmake ${FLAGS} ..
+make -j$NUM_THREADS
+sudo make -j$NUM_THREADS install
