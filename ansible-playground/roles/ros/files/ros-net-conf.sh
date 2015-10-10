8abd136718b55f8a15489f5b0f6f2ba49b41884b
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-10-10 21:56:20 +0300

    Changed ros network configurations -- use ROS_HOSTNAME instead of ROS_IP

--- ansible-playground/roles/ros/files/ros-net-conf.sh
+++ ansible-playground/roles/ros/files/ros-net-conf.sh
@@ -4,4 +4,4 @@ ETH_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
 MASTER_IP=$(ip route | grep default | awk '{print $3}')
 MASTER_PORT=11311
 export ROS_MASTER_URI=http://${MASTER_IP}:11311
-export ROS_IP=${ETH_IP}
+export ROS_HOSTNAME=$(hostname)
--- host-pc/ros_net_conf/ros-net-conf-master.sh
+++ host-pc/ros_net_conf/ros-net-conf-master.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+##
+#   - ROS_MASTER_URI: Tells nodes where to they can locate the ROS Master process.
+#   - ROS_HOSTNAME: Sets the declared network address of a ROS Node or tool.
+#
+#   The master process can run on a different address than nodes, for example,
+#     the roslaunch server.
+#
+##
+
 ETH_IP=$(ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
 MASTER_PORT=11311
-export ROS_MASTER_URI=http://${ETH_IP}:${MASTER_PORT}
-export ROS_IP=${ETH_IP}
+HOST=$(hostname)
+export ROS_MASTER_URI=http://${HOST}:${MASTER_PORT}
+export ROS_HOSTNAME=${HOST}
 echo -e "\033[1;32mROS_MASTER_URI ---> ${ROS_MASTER_URI}\033[0m"
-echo -e "\033[1;32mROS_IP ---> ${ROS_IP}\033[0m"
+echo -e "\033[1;32mROS_HOSTNAME ---> ${ROS_HOSTNAME}\033[0m"
