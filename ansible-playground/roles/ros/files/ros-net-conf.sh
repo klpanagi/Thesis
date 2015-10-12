b8305ca63c06af3a871f245c7adf7a10c8535454
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-10-12 17:47:15 +0300

    Using hostnames instead of raw ip addresses

--- ansible-playground/roles/ros/files/ros-net-conf.sh
+++ ansible-playground/roles/ros/files/ros-net-conf.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-ETH_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
-MASTER_IP=$(ip route | grep default | awk '{print $3}')
+#ETH_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+#MASTER_IP=$(ip route | grep default | awk '{print $3}')
+
 MASTER_PORT=11311
-export ROS_MASTER_URI=http://${MASTER_IP}:11311
-export ROS_HOSTNAME=$(hostname)
+MASTER_HOSTNAME='master-device'  ## hostname where ROS-MASTER is running.
+NODES_HOSTNAME=$(hostname)  ## Ros nodes will run on this host.
+
+export ROS_MASTER_URI=http://${MASTER_HOSTNAME}:11311
+export ROS_HOSTNAME=${NODES_HOSTNAME}
+
+echo -e "\033[0;33mROS_MASTER_URI ---> ${ROS_MASTER_URI}\033[0m"
+echo -e "\033[0;33mROS_HOSTNAME ---> ${ROS_HOSTNAME}\033[0m"
--- host-pc/ros_net_conf/ros-net-conf-master.sh
+++ host-pc/ros_net_conf/ros-net-conf-master.sh
@@ -7,12 +7,19 @@
 #   The master process can run on a different address than nodes, for example,
 #     the roslaunch server.
 #
+#   Comments:
+#   - ROS_MASTER_URI cannot be assigned at a public address (10.1.1.1) while ROS_HOSTNAME is defined at localhost.
+#   - 
 ##
 
-ETH_IP=$(ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+#ETH_IP=$(ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+
 MASTER_PORT=11311
-HOST=$(hostname)
-export ROS_MASTER_URI=http://${HOST}:${MASTER_PORT}
-export ROS_HOSTNAME=${HOST}
+MASTER_HOSTNAME='master-device'
+NODES_HOSTNAME='master-device'
+
+export ROS_MASTER_URI=http://${MASTER_HOSTNAME}:${MASTER_PORT}
+export ROS_HOSTNAME=${NODES_HOSTNAME}
+
 echo -e "\033[1;32mROS_MASTER_URI ---> ${ROS_MASTER_URI}\033[0m"
 echo -e "\033[1;32mROS_HOSTNAME ---> ${ROS_HOSTNAME}\033[0m"
