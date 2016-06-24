22c834e0cd18573c83e758d6bb4cd8a36afad0cf
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-06-24 17:55:38 +0300

    cuda-repo-l4t-r21.3-6-5-prod_6.5-42_armhf installation shell script

--- jetson-tk1/cuda/cuda-6.5.sh
+++ jetson-tk1/cuda/cuda-6.5.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-URL=http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda-repo-l4t-r21.3-6-5-prod_6.5-34_armhf.deb
+URL="http://developer.download.nvidia.com/embedded/L4T/r21_Release_v3.0/cuda-repo-l4t-r21.3-6-5-prod_6.5-42_armhf.deb"
 
-#wget ${URL}
+cd /tmp
+wget ${URL}
 
-sudo dpkg --force-all -i cuda-repo-l4t-r21.3-6-5-prod_6.5-34_armhf.deb
-sudo sed -i.bak 's/\(^deb.*main restricted\)\s*$/\1 universe multiverse/g' /etc/apt/sources.list
-sudo sed -i.bak 's/\(^deb.*main restricted universe\)\s*$/\1 multiverse/g' /etc/apt/sources.list
+sudo dpkg -i cuda-repo-l4t-r21.3-6-5-prod_6.5-42_armhf.deb
 sudo apt-get -y update
-sudo apt-get -y --force-yes install cuda-toolkit-6.5 libgomp1 libfreeimage-dev libopenmpi-dev openmpi-bin
-grep -q "export PATH=.*/usr/local/cuda-6.5/bin" ~/.bashrc || echo "export PATH=/usr/local/cuda-6.5/bin:$PATH">>~/.bashrc
+sudo apt-get -y install cuda-toolkit-6-5
 
-grep -q "export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib" ~/.bashrc || echo "export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+#grep -q "export PATH=.*/usr/local/cuda-6.5/bin" ~/.bashrc || echo "export PATH=/usr/local/cuda-6.5/bin:$PATH">>~/.bashrc
 
-#grep -q "export __GL_PERFORM_MODE=1" ~/.bashrc || echo "export __GL_PERFORM_MODE=1"
+#grep -q "export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib" ~/.bashrc || echo "export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
 
-source ~/.bashrc
+#source ~/.bashrc
