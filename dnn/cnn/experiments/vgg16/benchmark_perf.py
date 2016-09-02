2868d60d7cc7f2ebcc232f5be40b8ffb3d028885
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-09-02 16:42:55 +0300

    Performance results for vgg16 on intel i7

--- dnn/cnn/experiments/vgg16/benchmark_perf.py
+++ dnn/cnn/experiments/vgg16/benchmark_perf.py
@@ -2,7 +2,8 @@
 import sys
 from Benchmark import Benchmark
 from Models import VGG16
-from keras import backend as K
+from os import path
+import os
 
 if __name__ == "__main__":
     try:
@@ -13,9 +14,10 @@ if __name__ == "__main__":
     # Create the model instance
     vgg16 = VGG16()
     # Load weights
-    vgg16.load_weights("vgg16_weights.h5")
+    wpath = path.join(os.getcwd(), '../../Models/weights/vgg16_th.weights')
+    vgg16.load_weights(wpath)
     vgg16.compile()
-    model = vgg16.get_model()
+    # model = vgg16.get_model()
     # Generate function to visualize first layer
     # convout1_f = K.function([model.layers[0].input, K.learning_phase()], [model.layers[7].output])
     # _inData = load_image_to_Tensor4D('cat.jpg', (224, 224))
@@ -31,4 +33,4 @@ if __name__ == "__main__":
     benchmarker = Benchmark(vgg16)
     benchmarker.run_for_performance('cat.jpg', iterations=iterations)
     benchmarker.save_results()
-#     benchmarker.create_figure()
+    benchmarker.create_figure()
